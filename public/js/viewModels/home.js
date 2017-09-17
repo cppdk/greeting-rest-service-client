cdca43f90453c520774562d59f256e07eabc89f0
kstniels <kstniels@KSTNIELS-DK.dk.oracle.com>
2017-09-17 19:35:39 +0200

    Feature 2 implementation

--- public/js/viewModels/home.js
+++ public/js/viewModels/home.js
@@ -1,7 +1,20 @@
 define(['ojs/ojcore', 'knockout', 'ojs/ojmodel', 'ojs/ojtable', 'ojs/ojbutton'],
 function(oj, ko) {
 
-  var viewModel = {
+    function getData(data, host, port) {
+         $.getJSON("http://"+host+":"+port+"/greetings").
+                then(function (json) {
+                    data = this.greeting;
+                    });  
+         
+    };
+
+  function viewModel() {
+      var self = this;
+      self.value = ko.observable("Somewhat long value");
+      self.host = envOptions.host;
+      self.port = envOptions.port;
+      getData(self.value, self.host, self.port);
   };
 
   return viewModel;
---
+++ public/resource/envconfig.js
@@ -0,0 +1,6 @@
+envOptions = {
+  // This can have nested stuff, arrays, etc.
+  host: "localhost",
+  port: "8080"
+}
+
